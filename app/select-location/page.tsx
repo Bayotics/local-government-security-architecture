649e95b2fe2d35cdf29a5d426d5814c0d9bad8b2
Abdullahi Shobaloju <sabdullahi@cinnsol.com>
2025-10-27 21:07:01 +0100

    force dynamic loading

--- app/select-location/page.tsx
+++ app/select-location/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+export const dynamic = "force-dynamic"
 
 import { useState, useEffect } from "react"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
@@ -242,4 +243,4 @@ export default function SelectLocation() {
       </div>
     </>
   )
-} // end 
+}
